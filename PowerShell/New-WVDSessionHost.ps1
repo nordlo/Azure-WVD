ddfb93df64491dc78c97751adc275c8fce68fb91
joabra <63706825+joabra@users.noreply.github.com>
2021-03-10 19:58:58 +0100

    Update New-WVDSessionHost.ps1

--- PowerShell/New-WVDSessionHost.ps1
+++ PowerShell/New-WVDSessionHost.ps1
@@ -61,7 +61,7 @@ $Win7x64_WVDBootMgrURI   = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/b
 #    Test/Create Temp Directory    #
 ####################################
 if((Test-Path c:\temp) -eq $false) {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp Directory"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Create C:\temp Directory"
     Write-Host `
         -ForegroundColor Cyan `
         -BackgroundColor Black `
@@ -69,14 +69,14 @@ if((Test-Path c:\temp) -eq $false) {
     New-Item -Path c:\temp -ItemType Directory
 }
 else {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp Already Exists"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "C:\temp Already Exists"
     Write-Host `
         -ForegroundColor Yellow `
         -BackgroundColor Black `
         "temp directory already exists"
 }
 if((Test-Path $LocalWVDpath) -eq $false) {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp\WVD Directory"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Create C:\temp\WVD Directory"
     Write-Host `
         -ForegroundColor Cyan `
         -BackgroundColor Black `
@@ -84,15 +84,15 @@ if((Test-Path $LocalWVDpath) -eq $false) {
     New-Item -Path $LocalWVDpath -ItemType Directory
 }
 else {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp\WVD Already Exists"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "C:\temp\WVD Already Exists"
     Write-Host `
         -ForegroundColor Yellow `
         -BackgroundColor Black `
         "c:\temp\wvd directory already exists"
 }
-New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+New-Item -Path c:\ -Name New-Nordlo-WVDSessionHost.log -ItemType File
 Add-Content `
--LiteralPath C:\New-WVDSessionHost.log `
+-LiteralPath C:\New-Nordlo-WVDSessionHost.log `
 "
 ProfilePath       = $ProfilePath
 RegistrationToken = $RegistrationToken
@@ -103,18 +103,18 @@ Optimize          = $Optimize
 #################################
 #    Download WVD Componants    #
 #################################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Boot Loader"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Downloading WVD Boot Loader"
     Invoke-WebRequest -Uri $WVDBootURI -OutFile "$LocalWVDpath$WVDBootInstaller"
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading FSLogix"
-    Invoke-WebRequest -Uri $FSLogixURI -OutFile "$LocalWVDpath$FSInstaller"
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Agent"
+#Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Downloading FSLogix"
+    #Invoke-WebRequest -Uri $FSLogixURI -OutFile "$LocalWVDpath$FSInstaller"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Downloading WVD Agent"
     Invoke-WebRequest -Uri $WVDAgentURI -OutFile "$LocalWVDpath$WVDAgentInstaller"
 
-
+<#
 ##############################
 #    Prep for WVD Install    #
 ##############################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Unzip FSLogix"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Unzip FSLogix"
 Expand-Archive `
     -LiteralPath "C:\temp\wvd\$FSInstaller" `
     -DestinationPath "$LocalWVDpath\FSLogix" `
@@ -122,15 +122,15 @@ Expand-Archive `
     -Verbose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 cd $LocalWVDpath 
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "UnZip FXLogix Complete"
-
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "UnZip FXLogix Complete"
+#>
 
 ##############################
 #    OS Specific Settings    #
 ##############################
 $OS = (Get-WmiObject win32_operatingsystem).name
 If(($OS) -match 'server') {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows Server OS Detected"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Windows Server OS Detected"
     write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Server OS Detected"
     If(((Get-WindowsFeature -Name RDS-RD-Server).installstate) -eq 'Installed') {
         "Session Host Role is already installed"
@@ -151,14 +151,14 @@ If(($OS) -match 'server') {
     $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)    
 }
 Else {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows Client OS Detected"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Windows Client OS Detected"
     write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Client OS Detected"
     if(($OS) -match 'Windows 10') {
         write-host `
             -ForegroundColor Yellow `
             -BackgroundColor Black  `
             "Windows 10 detected...skipping to next step"
-        Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows 10 Detected...skipping to next step"     
+        Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Windows 10 Detected...skipping to next step"     
     }    
     else {
         $OSArch = (Get-WmiObject win32_operatingsystem).OSArchitecture
@@ -167,7 +167,7 @@ Else {
                 -ForegroundColor Magenta  `
                 -BackgroundColor Black `
                 "Windows 7 x64 detected"
-            Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows 7 x64 Detected"
+            Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Windows 7 x64 Detected"
 
 
             #################################
@@ -205,7 +205,7 @@ Else {
 ################################
 #    Install WVD Componants    #
 ################################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Bootloader"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Installing WVD Bootloader"
 $bootloader_deploy_status = Start-Process `
     -FilePath "msiexec.exe" `
     -ArgumentList "/i $WVDBootInstaller", `
@@ -217,10 +217,10 @@ $bootloader_deploy_status = Start-Process `
     -Wait `
     -Passthru
 $sts = $bootloader_deploy_status.ExitCode
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Bootloader Complete"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Installing WVD Bootloader Complete"
 Write-Output "Installing RDAgentBootLoader on VM Complete. Exit code=$sts`n"
 Wait-Event -Timeout 5
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Agent"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Installing WVD Agent"
 Write-Output "Installing RD Infra Agent on VM $AgentInstaller`n"
 $agent_deploy_status = Start-Process `
     -FilePath "msiexec.exe" `
@@ -232,14 +232,14 @@ $agent_deploy_status = Start-Process `
         "REGISTRATIONTOKEN=$RegistrationToken", "/l* $LocalWVDpath\AgentInstall.txt" `
     -Wait `
     -Passthru
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "WVD Agent Install Complete"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "WVD Agent Install Complete"
 Wait-Event -Timeout 5
 
-
+<#
 #########################
 #    FSLogix Install    #
 #########################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing FSLogix"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Installing FSLogix"
 $fslogix_deploy_status = Start-Process `
     -FilePath "$LocalWVDpath\FSLogix\x64\Release\FSLogixAppsSetup.exe" `
     -ArgumentList "/install /quiet" `
@@ -250,7 +250,7 @@ $fslogix_deploy_status = Start-Process `
 #######################################
 #    FSLogix User Profile Settings    #
 #######################################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Configure FSLogix Profile Settings"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Configure FSLogix Profile Settings"
 Push-Location 
 Set-Location HKLM:\SOFTWARE\
 New-Item `
@@ -309,12 +309,12 @@ Set-ItemProperty `
     -Type DWord `
     -Value 1
 Pop-Location
-
+#>
 
 ##########################################
 #    Enable Screen Capture Protection    #
 ##########################################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Enable Screen Capture Protection"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Enable Screen Capture Protection"
 Push-Location 
 Set-Location "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
 New-ItemProperty `
@@ -325,7 +325,7 @@ New-ItemProperty `
     -Force
 Pop-Location
 
-
+<#
 ##############################################
 #    WVD Optimizer (Virtual Desktop Team)    #
 ##############################################
@@ -334,8 +334,8 @@ If ($Optimize -eq $true) {
     ################################
     #    Download WVD Optimizer    #
     ################################
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize Selected"
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Creating C:\Optimize folder"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Optimize Selected"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Creating C:\Optimize folder"
     New-Item -Path C:\ -Name Optimize -ItemType Directory -ErrorAction SilentlyContinue
     $LocalOptimizePath = "C:\Optimize\"
     $WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
@@ -348,7 +348,7 @@ If ($Optimize -eq $true) {
     ###############################
     #    Prep for WVD Optimize    #
     ###############################
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize downloaded and extracted"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Optimize downloaded and extracted"
     Expand-Archive `
         -LiteralPath "C:\Optimize\Windows_10_VDI_Optimize-master.zip" `
         -DestinationPath "$LocalOptimizePath" `
@@ -360,19 +360,19 @@ If ($Optimize -eq $true) {
     #################################
     #    Run WVD Optimize Script    #
     #################################
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Begining Optimize"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Begining Optimize"
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
     .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimization Complete"
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Optimization Complete"
 }
 else {
     Write-Output "Optimize not selected"
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize NOT selected"    
+    Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Optimize NOT selected"    
 }
-
+#>
 
 ##########################
 #    Restart Computer    #
 ##########################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Process Complete - REBOOT"
+Add-Content -LiteralPath C:\New-Nordlo-WVDSessionHost.log "Process Complete - REBOOT"
 Restart-Computer -Force 
